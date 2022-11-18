c945eed27eba8f8e80398df12d10b7b271b8c309
Andrey Sysoev <andrey@sysoev.ru>
2022-11-18 13:36:15 +0100

    sprint_08: added colors

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Andrey Sysoev on 18.11.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ypBackground: UIColor = UIColor(named: "YP Background")!
+    static let ypBlack: UIColor = UIColor(named: "YP Black")!
+    static let ypBlue: UIColor = UIColor(named: "YP Blue")!
+    static let ypGray: UIColor = UIColor(named: "YP Gray")!
+    static let ypRed: UIColor = UIColor(named: "YP Red")!
+    static let ypWhiteTransparent: UIColor = UIColor(named: "YP White (Alpha 50)")!
+    static let ypWhite: UIColor = UIColor(named: "YP White")!
+}
