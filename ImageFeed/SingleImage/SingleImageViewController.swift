0616d15f4ecc86d7694f49a54da218770d5dedff
Andrey Sysoev <andrey@sysoev.ru>
2022-12-01 17:40:59 +0100

    spint_09: added SingleImageViewController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Andrey Sysoev on 01.12.2022.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+    
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true)
+    }
+}
