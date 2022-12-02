1e42f908a4b7b581b881b439082cfbb3492873dc
Andrey Sysoev <andrey@sysoev.ru>
2022-12-02 15:00:09 +0100

    sprint_09: replaced Profile layout made in storyboard with code in ProfileViewController

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,17 +8,104 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private weak var profileImage: UIImageView!
-    @IBOutlet private weak var usernameLabel: UIStackView!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    @IBOutlet private weak var nameLabel: UILabel!
+    // MARK: - Views
+    
+    private var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "ProfileImage")
+        imageView.layer.cornerRadius = 35
+        return imageView
+    }()
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Екатерина Новикова"
+        label.font = UIFont(name: "YSDisplay-Bold", size: 23)
+        label.textColor = .ypWhite
+        return label
+    }()
+    private var usernameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "@ekaterina_nov"
+        label.font = UIFont(name: "YSDisplay-Medium", size: 13)
+        label.textColor = .ypGray
+        return label
+    }()
+    private var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, world!"
+        label.font = UIFont(name: "YSDisplay-Medium", size: 13)
+        label.textColor = .ypWhite
+        return label
+    }()
+    private lazy var logoutButton: UIButton = {
+        let button = UIButton.systemButton(
+            with: UIImage(named: "logout")!,
+            target: self,
+            action: #selector(didTapLogoutButton)
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .ypRed
+        return button
+    }()
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupContent()
+        setupConstraints()
     }
     
-    @IBAction private func didTapLogoutButton() {
+    // MARK: - Methods
+    
+    private func setupContent() {
+        view.addSubview(profileImage)
+        view.addSubview(logoutButton)
+        view.addSubview(nameLabel)
+        view.addSubview(usernameLabel)
+        view.addSubview(descriptionLabel)
+    }
+    
+    private func setupConstraints() {
+        let profileImageConstraints = [
+            profileImage.widthAnchor.constraint(equalToConstant: 70),
+            profileImage.heightAnchor.constraint(equalToConstant: 70),
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        ]
+        let logoutButtonConstraints = [
+            logoutButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26)
+        ]
+        let nameLabelConstraints = [
+            nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor)
+        ]
+        let usernameLabelConstraints = [
+            usernameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            usernameLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor)
+        ]
+        let descriptionLabelConstraints = [
+            descriptionLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: profileImage.leadingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(
+            profileImageConstraints +
+            logoutButtonConstraints +
+            nameLabelConstraints +
+            usernameLabelConstraints +
+            descriptionLabelConstraints
+        )
+    }
+    
+    @objc
+    private func didTapLogoutButton() {
+        print("logout")
     }
 }
