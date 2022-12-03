e33bb2e3b3620a72f6abef95e69f6a490b286971
Andrey Sysoev <andrey@sysoev.ru>
2022-12-03 23:11:36 +0100

    sprint_09: added guard let construction to prevent app crash

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -27,9 +27,11 @@ final class ImagesListViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == ShowSingleImageSegueIdentifier {
-            let viewController = segue.destination as! SingleImageViewController
+            guard let viewController = segue.destination as? SingleImageViewController else { return }
+
             let indexPath = sender as! IndexPath
             let image = UIImage(named: imageNames[indexPath.row])
+
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
