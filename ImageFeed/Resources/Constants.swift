d72aa434e1dd2245b4f7f89fb18a6f9c582dae8f
Andrey Sysoev <andrey@sysoev.ru>
2022-12-21 10:56:44 +0300

    sprint_10: added Constants

---
+++ ImageFeed/Resources/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Andrey Sysoev on 15.12.2022.
+//
+
+import Foundation
+
+let AccessKey = "x_1HVdzZP5QZYmnJuyrc6k5KC6ShRuuskFBHu7A2p5Y"
+
+let SecretKey = "85T5gNlU9cpY1BRHNmDWC-jxW3jx0iEotjpLYty3MGQ"
+
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+let AccessScope = "public+read_user+write_likes"
+
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
